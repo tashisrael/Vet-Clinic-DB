7a1f4054cb39755949be0bbac7afec308d5561fd
tashisrael <tashisrael04@gmail.com>
2022-12-13 18:27:10 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE vet_clinic
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+CREATE TABLE public.animals
+(
+    id integer NOT NULL,
+    name character varying(100),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric,
+    PRIMARY KEY (id)
 );
+
